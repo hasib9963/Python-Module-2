9eafb6f3ae399704b42c0574e795343e22a951cb
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-08-26 08:01:15 +0600

    Import and use functions with aliasing and wildcard imports, demonstrating the use of double_it and full_name functions from different modules.

---
+++ modules.py
@@ -0,0 +1,9 @@
+from function import double_it as dt
+from kargs_multiple import full_name as name
+from default_args import * 
+
+f_name = name('tomato', 'Morich')
+print(f_name)
+# result = double_it(45)
+result = dt(45)
+# print(result)
